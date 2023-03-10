01606ef86c322e03de7286c1a399e022f56674e9
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-03-11 00:54:10 +0100

    solution for module 2, task 1, 2

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,24 @@
+//'use strict'
+
 let calculator = {
   // ваш код
+  read() {
+    this.a = +prompt("Enter a value", 0);
+    this.b = +prompt("Enter b value", 0);
+  },
+
+  sum() {
+    console.log(this.a, this.b);
+    console.log(this.a + this.b);
+    return this.a + this.b;
+  },
+  mul() {
+    return this.a * this.b;
+  },
 };
 
+//calculator.read();
+//console.log(calculator.sum());
+//console.log(calculator.mul());
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
