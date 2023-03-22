24604cce941cfad8c017b2e6d6e83feec7e566af
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-03-22 23:58:09 +0100

    solution 4-module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
   // ваш код...
+  let rows = table.rows;
+
+  for (let i = 1; i < rows.length; i++) {
+    let cells = rows[i].cells;
+    if (!cells[3].hasAttribute("data-available")) {
+      rows[i].setAttribute("hidden", '');
+    }
+    else {
+      if (cells[3].dataset.available === 'true') {
+        rows[i].classList.add("available");
+      } else {
+        rows[i].classList.add("unavailable");
+      }
+    }
+
+    if (cells[2].textContent === 'm') {
+      rows[i].classList.add('male');
+    }
+    else if (cells[2].textContent === 'f') {
+      rows[i].classList.add("female");
+    }
+
+    if (+cells[1].textContent < 18) {
+      rows[i].style.textDecoration = 'line-through';
+    }
+  }
 }
