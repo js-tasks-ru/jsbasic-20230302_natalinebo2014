60f12a0bdc70f6cb872e01ac97b9508bb20a5d16
Krylova Nataliya <natalinebo2014@yandex.ru>
2023-03-31 18:14:54 +0200

    solution module 5

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,9 @@
 function hideSelf() {
   // ваш код...
+  const hideSelfBtn = document.querySelector(".hide-self-button");
+
+  hideSelfBtn.addEventListener('click', function() {
+    console.log(this)
+    this.setAttribute('hidden', '');
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
   // ваш код...
+  const carouselInner = document.querySelector(".carousel__inner");
+  const carouselSlides = document.querySelectorAll(".carousel__slide");
+  const carouselSlide = document.querySelector(".carousel__slide");
+  const carouselBtns = document.querySelectorAll(".carousel__arrow");
+  const leftBtn = document.querySelector(".carousel__arrow_left");
+  const rightBtn = document.querySelector(".carousel__arrow_right");
+
+  let offsetValue = carouselSlide.offsetWidth;
+  let currentSlide = 0;
+
+  if (currentSlide === 0) {
+    leftBtn.style.display = "none";
+  }
+
+  leftBtn.addEventListener('click', function() {
+    currentSlide--;
+    let shiftValue = offsetValue * currentSlide;
+
+    carouselInner.style.transform = `translateX(-${shiftValue}px)`;
+    carouselBtns.forEach(btn => btn.style.display = '');
+
+    if (currentSlide <= 0) {
+      this.style.display = 'none';
+    }
+  });
+
+
+  rightBtn.addEventListener("click", function () {
+    currentSlide++;
+    let shiftValue = offsetValue * currentSlide;
+    carouselInner.style.transform = `translateX(-${shiftValue}px)`;
+    carouselBtns.forEach((btn) => (btn.style.display = ""));
+
+    if (currentSlide >= carouselSlides.length - 1) {
+      this.style.display = "none";
+    }
+  }); 
 }
